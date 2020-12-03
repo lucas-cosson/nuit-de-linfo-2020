2e405fd750242161b3499bb65a4e8aca61853f69
lucas-cosson <evox.neo@gmail.com>
2020-12-04 00:42:36 +0100

    change interface + setup api

---
+++ client/js/index.js
@@ -0,0 +1,29 @@
+const waterman = document.getElementById("waterman");
+const lieu = document.getElementById("lieu");
+const date_session = document.getElementById("date-session");
+const time = document.getElementById("time");
+const polution = document.getElementById("polution");
+const polution_value = document.getElementById("polution-value");
+
+polution.addEventListener(
+  "input",
+  () => {
+    polution_value.innerHTML = `${polution.value}%`;
+  },
+  false
+);
+
+document.getElementById("submit-data").addEventListener("click", async () => {
+  const init = {
+    method: "GET",
+    body: JSON.stringify({
+      waterman: waterman.value,
+      lieu: lieu.value,
+      date_session: date_session.value,
+      time: time.value,
+      polution: polution.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  };
+  const response = await fetch(`/database/`, init);
+});
