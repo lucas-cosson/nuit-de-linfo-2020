c6c1ea19c02301fdeeb88aa6ff062e3324492019
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-03 23:23:43 +0100

    transformation des lettres

---
+++ client/js/leet_speak.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const leetSpeakData = {
+    activated: false,
+    html: document.body.innerHTML,
+    alphabet: {
+      3: /i/gi,
+    },
+  };
+
+  function leetSpeak() {
+    if (leetSpeakData.activated) {
+      leetSpeakData.activated = false;
+      document.body.innerHTML = leetSpeakData.html;
+      return;
+    }
+
+    leetSpeakData.activated = true;
+
+    for (let child of document.body.childNodes) {
+      if (child.innerText !== undefined) {
+        child.innerText = child.innerText.replaceAll(/i/gi, "3");
+        child.innerText = child.innerText.replaceAll(/s/gi, "5");
+        child.innerText = child.innerText.replaceAll(/t/gi, "7");
+      }
+    }
+  }
+
+  document.getElementById("leet-speak").addEventListener("click", leetSpeak);
+});
