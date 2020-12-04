6b277a3f40f20e74d28a089072f4c2e6c304da5b
Miyne <viprey.pierre@gmail.com>
2020-12-04 05:07:36 +0100

    GLHF

--- client/js/index.js
+++ client/js/index.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     let video=document.getElementById('autoplay');
+    let root=document.getElementsByClassName('premier');
+    let i = root.length;
     video.addEventListener("mouseover", function(){
         video.play();
+        let j=i;
+        while(j--){
+            root[j].className += ' enBas';
+        }
     });
 
-    video.addEventListener("mouseout", function(){
+    let nav=document.getElementById('unroll');
+    nav.addEventListener("onmouseout", function(){
         video.pause();
-    });    
+        let j=i;
+        while(j--){
+            root[j].classList.remove('enBas');
+        }
+    });
 });
