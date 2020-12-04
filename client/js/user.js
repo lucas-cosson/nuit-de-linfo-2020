31dfc7069b65fd6cb90eb8a378f6d829e64e4ffa
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-04 05:37:42 +0100

    Connexion

--- client/js/user.js
+++ client/js/user.js
@@ -1,14 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const registerBtn = document.getElementById("submit-inscription");
-  registerBtn.addEventListener("click", (ev) => {
+  document
+    .getElementById("submit-inscription")
+    .addEventListener("click", (ev) => {
+      ev.preventDefault();
+
+      const data = {
+        user: document.getElementById("re_name").value,
+        password: document.getElementById("re_password").value,
+      };
+
+      fetch("/api/user/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    });
+
+  document.getElementById("submit-connect").addEventListener("click", (ev) => {
     ev.preventDefault();
 
     const data = {
-      user: document.getElementById("re_name").value,
-      password: document.getElementById("re_password").value,
+      user: document.getElementById("co_name").value,
+      password: document.getElementById("co_password").value,
     };
 
-    fetch("/api/user/register", {
+    fetch("/api/user/connect", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const bcryptjs = require("bcryptjs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,8 +16,6 @@ app.get("index.html", (_, res) => {
 });
 
 app.post("/api/user/register", (req, res) => {
-  console.log("hey");
-
   const connection = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "",
@@ -27,17 +26,46 @@ app.post("/api/user/register", (req, res) => {
   connection.connect();
 
   const user = req.body.user;
-  const password = req.body.password;
+  const password = bcryptjs.hashSync(req.body.password, 10);
 
   connection.query(
     `INSERT INTO user (us_name, us_password) VALUES ('${user}', '${password}')`,
-    (err, res) => {
+    (err, _) => {
       if (err) throw err;
-      console.log(res);
     }
   );
 
-  res.status(200);
+  res.end("Nouvel utilisateur");
+});
+
+app.post("/api/user/connect", (req, res) => {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PWD || "",
+    database: process.env.DB_DATABASE || "",
+  });
+
+  connection.connect();
+
+  const user = req.body.user;
+  let password;
+
+  connection.query(
+    `SELECT us_password FROM user WHERE us_name='${user}'`,
+    (error, result) => {
+      if (error) throw err;
+      if (result === []) return;
+
+      password = result[0].us_password;
+      const auth = bcryptjs.compareSync(req.body.password, password);
+
+      if (auth) {
+        res.end("Connecté");
+        console.log("connecté");
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
