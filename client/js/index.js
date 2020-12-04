66d2522f9280da3185a519e93f8361e89710e206
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-04 07:46:30 +0100

    darkmode

--- client/js/index.js
+++ client/js/index.js
@@ -1,76 +1,84 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("darkmode").addEventListener("click", () => {
+    alert("Nous sommes en rupture de stock de dark mode.");
+  });
 
-document.addEventListener("DOMContentLoaded", function() {
-    let video=document.getElementById('autoplay');
-    let root=document.getElementsByClassName('premier');
-    let i = root.length;
-    video.addEventListener("mouseover", function(){
-        video.play();
-        let j=i;
-        while(j--){
-            root[j].className += ' enBas';
-        }
-    });
+  let video = document.getElementById("autoplay");
+  let root = document.getElementsByClassName("premier");
+  let i = root.length;
+  video.addEventListener("mouseover", function () {
+    video.play();
+    let j = i;
+    while (j--) {
+      root[j].className += " enBas";
+    }
+  });
 
-    let nav=document.getElementById('unroll');
-    nav.addEventListener("onmouseout", function(){
-        video.pause();
-        let j=i;
-        while(j--){
-            root[j].classList.remove('enBas');
-        }
-    });
+  let nav = document.getElementById("unroll");
+  nav.addEventListener("onmouseout", function () {
+    video.pause();
+    let j = i;
+    while (j--) {
+      root[j].classList.remove("enBas");
+    }
+  });
 
-    const waterman = document.getElementById("waterman");
-    const lieu = document.getElementById("lieu");
-    const date_session = document.getElementById("date-session");
-    const time = document.getElementById("time");
-    const pollution = document.getElementById("pollution");
-    const pollution_value = document.getElementById("pollution-value");
+  const waterman = document.getElementById("waterman");
+  const lieu = document.getElementById("lieu");
+  const date_session = document.getElementById("date-session");
+  const time = document.getElementById("time");
+  const pollution = document.getElementById("pollution");
+  const pollution_value = document.getElementById("pollution-value");
 
-    pollution.addEventListener(
+  pollution.addEventListener(
     "input",
     () => {
-        pollution_value.innerHTML = `${pollution.value}%`;
+      pollution_value.innerHTML = `${pollution.value}%`;
     },
     false
-    );
+  );
 
-    window.addEventListener ("load", function(){
+  window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
     setTimeout(suiteTraitement, 5000);
-    function suiteTraitement()
-    {
-    loader.className+=" hidden";
+    function suiteTraitement() {
+      loader.className += " hidden";
     }
+  });
 
-    });
-
-document.getElementById("submit-data").addEventListener("click", async () => {
-    if(waterman.value === "neo" || waterman.value === "Neo"){
-        document.getElementById("easterEgg").children[0].classList.remove("hidden");
-        waterman.value = "";
-        return;
+  document.getElementById("submit-data").addEventListener("click", async () => {
+    if (waterman.value === "neo" || waterman.value === "Neo") {
+      document
+        .getElementById("easterEgg")
+        .children[0].classList.remove("hidden");
+      waterman.value = "";
+      return;
     }
     const init = {
-    method: "POST",
-    body: JSON.stringify({
-      waterman: waterman.value,
-      lieu: lieu.value,
-      date_session: date_session.value,
-      time: time.value,
-      pollution: pollution.value,
-    }),
-    headers: { "Content-Type": "application/json" },
-  };
+      method: "POST",
+      body: JSON.stringify({
+        waterman: waterman.value,
+        lieu: lieu.value,
+        date_session: date_session.value,
+        time: time.value,
+        pollution: pollution.value,
+      }),
+      headers: { "Content-Type": "application/json" },
+    };
 
     await fetch(`/api/form/insert`, init);
-    });
+  });
 
-    // EASTER EGG
-    document.getElementById("easterEgg").addEventListener("click", () => {
-        document.getElementById("easterEgg").children[0].classList.add("hidden");
-    });
-    document.getElementById("easterEgg").querySelector("button:nth-of-type(2)").addEventListener("click", () => {
-        document.getElementById("leet-speak").children[0].classList.remove("hidden");
+  // EASTER EGG
+  document.getElementById("easterEgg").addEventListener("click", () => {
+    document.getElementById("easterEgg").children[0].classList.add("hidden");
+  });
+  document
+    .getElementById("easterEgg")
+    .querySelector("button:nth-of-type(2)")
+    .addEventListener("click", () => {
+      document
+        .getElementById("leet-speak")
+        .children[0].classList.remove("hidden");
     });
 });
