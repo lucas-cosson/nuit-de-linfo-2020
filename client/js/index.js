62d2230d97ed8d3db21b71b36a5540d37c5749cd
FabianCMI <fabian.devel@laposte.net>
2020-12-04 07:59:52 +0100

    yolo

--- client/js/index.js
+++ client/js/index.js
@@ -1,31 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let video = document.getElementById("autoplay");
-  let root = document.getElementsByClassName("premier");
-  let i = root.length;
-  video.addEventListener("mouseover", function () {
-    video.play();
-    let j = i;
-    while (j--) {
-      root[j].className += " enBas";
-    }
-  });
 
-  let nav = document.getElementById("unroll");
-  nav.addEventListener("onmouseout", function () {
-    video.pause();
-    let j = i;
-    while (j--) {
-      root[j].classList.remove("enBas");
-    }
-  });
-
-  const waterman = document.getElementById("waterman");
-  const lieu = document.getElementById("lieu");
-  const date_session = document.getElementById("date-session");
-  const time = document.getElementById("time");
-  const pollution = document.getElementById("pollution");
-  const pollution_value = document.getElementById("pollution-value");
-  
 document.addEventListener("DOMContentLoaded", function() {
     let video=document.getElementById('autoplay');
     let root=document.getElementsByClassName('premier');
@@ -54,40 +27,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const pollution = document.getElementById("pollution");
     const pollution_value = document.getElementById("pollution-value");
 
-  pollution.addEventListener(
+    pollution.addEventListener(
     "input",
     () => {
-      pollution_value.innerHTML = `${pollution.value}%`;
+        pollution_value.innerHTML = `${pollution.value}%`;
     },
     false
-  );
+    );
 
-  window.addEventListener("load", function () {
+    window.addEventListener ("load", function(){
     const loader = document.querySelector(".loader");
     setTimeout(suiteTraitement, 5000);
-    function suiteTraitement() {
-      loader.className += " hidden";
+    function suiteTraitement()
+    {
+    loader.className+=" hidden";
     }
-  });
 
-  document.getElementById("submit-data").addEventListener("click", async () => {
-    if (waterman.value === "néo" || waterman.value === "Néo") {
-      console.log("bonjour néo");
-      document
-        .getElementById("easterEgg")
-        .children[0].classList.remove("hidden");
-    }
-    const init = {
-      method: "POST",
-      body: JSON.stringify({
-        waterman: waterman.value,
-        lieu: lieu.value,
-        date_session: date_session.value,
-        time: time.value,
-        pollution: pollution.value,
-      }),
-      headers: { "Content-Type": "application/json" },
-    };
     });
 
     // Nav bar 
@@ -104,8 +59,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     document.querySelector("#gauche .second:first-child").addEventListener("click", () => {
-        document.querySelector(".display-view").classList.toggle("display-view");
+        const doc = document.querySelector(".display-view");
         document.getElementById("formulaire").classList.toggle("display-view");
+        doc.classList.toggle("display-view");
     });
 
     document.getElementById("submit-data").addEventListener("click", async () => {
@@ -127,64 +83,13 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     await fetch(`/api/form/insert`, init);
-  });
-
-  // EASTER EGG
-  document.getElementById("easterEgg").addEventListener("click", () => {
-    document.getElementById("easterEgg").children[0].classList.add("hidden");
-  });
-  document
-    .getElementById("easterEgg")
-    .querySelector("button:nth-of-type(2)")
-    .addEventListener("click", () => {
-      document
-        .getElementById("leet-speak")
-        .children[0].classList.remove("hidden");
     });
 
-  /* WEATHER API */
-  const ville = document.getElementById("ville");
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async (pos) => {
-          var crd = pos.coords;
-          const proxyurl = "https://cors-anywhere.herokuapp.com/";
-          let url = `https://www.metaweather.com/api/location/search/?lattlong=${crd.latitude},${crd.longitude}`;
-          let response = await fetch(proxyurl + url);
-
-          response = await response.json();
-          ville.innerHTML = `Ville : ${response[0].title}`;
-
-          url = `https://www.metaweather.com/api/location/${
-            response[0].woeid
-          }${today()}`;
-
-          response = await fetch(proxyurl + url);
-          response = await response.json();
-          console.log(response);
-          const weather_api = document.getElementById("weather-api-data");
-          for (let i = 0; i < 2; i++) {
-            weather_api.innerHTML += `<img src="../img/weather/${response[i].weather_state_abbr}.svg" alt="${response[i].weather_state_abbr}"><p>Température : ${response[i].the_temp}C°<p>`;
-          }
-        },
-        (err) => {
-          console.error(err);
-        }
-      );
-    } else {
-      ville.innerHTML = "Geolocation is not supported by this browser.";
-    }
-  }
-
-  function today() {
-    let today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1;
-    var yyyy = today.getFullYear();
-    return `/${yyyy}/${mm}/${dd}/`;
-  }
-
-  getLocation();
+    // EASTER EGG
+    document.getElementById("easterEgg").addEventListener("click", () => {
+        document.getElementById("easterEgg").children[0].classList.add("hidden");
+    });
+    document.getElementById("easterEgg").querySelector("button:nth-of-type(2)").addEventListener("click", () => {
+        document.getElementById("leet-speak").children[0].classList.remove("hidden");
+    });
 });
